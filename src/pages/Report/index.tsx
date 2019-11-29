a7c0698ccca5a6f8e4dfbba7d7f14168d3487d60
Thomas Maier <tmaier@Maiers-MacBook-Pro-15.local>
2019-11-29 16:24:49 +0100

    Added Grid to Report Page

--- src/pages/Report/index.tsx
+++ src/pages/Report/index.tsx
@@ -11,9 +11,10 @@ import reportContext from '../../context/report-context'
 const Report: FC = () => {
   const { report } = useContext(reportContext)
 
+  let image = null
   if (report != null) {
     // @ts-ignore
-    const image = window.electron.fs.readFileSync(report.results[0].images[0].path)
+    image = window.electron.fs.readFileSync(report.results[0].images[0].path)
     console.log('Image', image)
   }
 
@@ -27,7 +28,68 @@ const Report: FC = () => {
           <Text margin={{ left: '0.25rem' }} color={colors['grey']} size="1.5rem">
             {report.url}
           </Text>
-          <Box>{/* <Image src={'data:image/jpeg;base64,' + image.toString('base64')} /> */}</Box>
+          <Box
+            width="100%"
+            margin="3rem 0 .75rem"
+            style={{
+              minHeight: `calc(${window.innerHeight}px -  13rem)`,
+              overflowY: 'auto',
+              overflowX: 'hidden'
+            }}
+          >
+            {/* <Image src={'data:image/jpeg;base64,' + image.toString('base64')} /> */}
+            <Box
+              width="100%"
+              height="200px"
+              margin={{ bottom: '1rem' }}
+              direction="row"
+              justify="between"
+              style={{ minHeight: 200 }}
+            >
+              <Box width="65%" height="100%" background="green" style={{ borderRadius: 15 }}>
+                {/* <Image
+                  width="90%"
+                  height="90%"
+                  fit="cover"
+                  src={'data:image/jpeg;base64,' + image.toString('base64')}
+                  style={{ borderRadius: 15 }}
+                /> */}
+              </Box>
+              <Box width="30%" height="100%" background="red" style={{ borderRadius: 15 }}>
+                Right
+              </Box>
+            </Box>
+            <Box
+              width="100%"
+              height="200px"
+              margin={{ bottom: '1rem' }}
+              direction="row"
+              justify="between"
+              style={{ minHeight: 200 }}
+            >
+              <Box width="30%" height="100%" background="red" style={{ borderRadius: 15 }}>
+                Left
+              </Box>
+              <Box width="65%" height="100%" background="green" style={{ borderRadius: 15 }}>
+                Right
+              </Box>
+            </Box>
+            <Box
+              width="100%"
+              height="200px"
+              margin={{ bottom: '1rem' }}
+              direction="row"
+              justify="between"
+              style={{ minHeight: 200 }}
+            >
+              <Box width="65%" height="100%" background="green" style={{ borderRadius: 15 }}>
+                Left
+              </Box>
+              <Box width="30%" height="100%" background="red" style={{ borderRadius: 15 }}>
+                Right
+              </Box>
+            </Box>
+          </Box>
         </Fragment>
       )}
     </Box>
