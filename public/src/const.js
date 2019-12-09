c5d8f8324d45729e6e867c75df952b1b7e07854f
Thomas Maier <tmaier@mac_clyding.gingco-nm.de>
2019-12-09 17:05:52 +0100

    Adjust Max Page Height

--- public/src/const.js
+++ public/src/const.js
@@ -26,7 +26,7 @@ module.exports = {
   MIN_HEIGHT: 650,
   MIN_WIDTH: 960,
   DEFAULT_RESOLUTION: { width: 1280, height: 960 },
-  MAX_PAGE_HEIGHT: 20000,
+  MAX_PAGE_HEIGHT: 10000,
 
   // Report
   WAIT_DURATION: 2000, // 2 Seconds
